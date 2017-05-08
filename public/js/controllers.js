a1620e257c9d3b5cae8409192035e4dc1b4c544b
stevenzhao <steven.zhao@scmp.com>
2017-05-08 17:36:03 +0800

    initialize the TodoList project structure

---
+++ public/js/controllers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('AppCtrl', function ($scope, $http) {
+
+    $http({
+      method: 'GET',
+      url: '/api/name'
+    }).
+    then(function (data, status, headers, config) {
+      $scope.name = data.name;
+    },function (data, status, headers, config) {
+      $scope.name = 'Error!';
+    });
+
+  }).
+  controller('MyCtrl1', function ($scope) {
+    // write Ctrl here
+
+  }).
+  controller('MyCtrl2', function ($scope) {
+    // write Ctrl here
+
+  });
