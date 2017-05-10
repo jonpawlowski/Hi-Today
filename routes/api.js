f96085e67dab6e4f8d5652ce3cd2fb91c223f250
stevenzhao <zhwdyx1201@gmail.com>
2017-05-10 12:54:12 +0800

    Update API, make nodemon watch route files and app.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 exports.name = function (req, res) {
   res.json({
@@ -22,6 +23,17 @@ router.use(function(req, res, next){
 })
 
 /**
+ * Some general functions for query db
+ * **/
+
+function getAll (req, res) {
+  req.task_db.find({}, function(err, data){
+    if(err) res.send(err);
+    res.json(data);
+  })
+}
+
+/**
  * Preprocess the params from route
  * **/
 
@@ -38,77 +50,63 @@ router.param('id', function(req, res, next, id){
 /**
  * url: /api/task
  * method: get
- * behaviour: Display all tasks
+ * behaviour: Display all tasks or filter by status
  * **/
 
 router.get('/', function(req, res, next){
-  req.task_db.find({}, function(err, data){
-    if(err) res.send(err);
-    res.json(data);
-  })
+  if(_.isEmpty(req.query)){
+    getAll(req, res);
+  }else{
+    if(req.query.status == 'false' || req.query.status == 'true' ){
+      req.task_db.find(req.query, function(err, data){
+        if(err) res.send(err);
+        res.json(data);
+      })
+    }else{
+      getAll(req, res);
+    }
+  }
 });
 
 /**
  * url: /api/task/create
  * method: create
- * behaviour: create a new task
+ * behaviour: create a new task, the name can not be duplicate,
+ * then fetch all the tasks
  * **/
 
-router.post('/create', function(req, res, next){
+router.post('/', function(req, res, next){
   if(req.body){
-  //  todo check post data
-    var temp = req.task_db(req.body);
-    if(req.task_db.find({name: temp.name}, function (err, data) {
+  //  todo check post data, should have a test function to check req.body data
+  //  todo before instance new document
+    if(req.task_db.find({name: req.body.name}, function (err, data) {
       if(!data.length){
+        var temp = req.task_db(req.body);
         temp.save(function (err) {
           if(err) res.send(err);
-          req.task_db.find({}, function(err, data){
-            if(err) res.send(err);
-            res.send(data);
-          })
-        })
+          getAll(req, res);
+        });
       }else{
-        req.task_db.find({}, function(err, data){
-          if(err) res.send(err);
-          res.send(data);
-        })
+        getAll(req, res);
       }
     }));
   }else{
     next();
   }
-})
-
-/**
- * url: /api/task/:status
- * method: get
- * behaviour: Find or delete all tasks by status
- * **/
-
-router.get('/status/:status', function(req, res, next){
-  if(req.taskStatus){
-    taskStatus = req.taskStatus;
-    req.task_db.find({status: taskStatus}, function(err, data){
-      if(err) res.send(err);
-      res.json(data);
-    })
-  }else{
-    next();
-  }
 });
 
-router.delete('/status/:status', function(req, res, next){
-  if(req.taskStatus){
-    taskStatus = req.taskStatus;
-    req.task_db.find({status: taskStatus}, function(err, data){
+/**
+ * url: /api/task
+ * method: delate
+ * behaviour: Delete one task by Id or delete all tasks by status
+ * **/
+
+router.delete('/', function(req, res, next){
+  if(!_.isEmpty(req.query)){
+    req.task_db.find(req.query, function(err, data){
       if(err) res.send(err);
       if(data){
-        req.task_db.deleteMany({status: taskStatus}).then(function(){
-          req.task_db.find({}).then(function(err, data){
-            if(err) res.send(err);
-            res.json(data);
-          })
-        })
+        req.task_db.deleteMany(req.query).then(getAll(req, res));
       }
     })
   }else{
