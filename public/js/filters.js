9ee78a1977bfde11293ef4fa4c8281bf6341d76e
Steven Zhao <zhwdyx1201@gmail.com>
2017-05-21 22:40:20 +0800

    Optimze the time display

--- public/js/filters.js
+++ public/js/filters.js
@@ -28,7 +28,7 @@ angular.module('myApp.filters', [])
     moment.tz.add("Asia/Shanghai|CST CDT|-80 -90|01010101010101010|-1c1I0 LX0 16p0 1jz0 1Myp0 Rb0 1o10 11z0 1o10 11z0 1qN0 11z0 1o10 11z0 1o10 11z0|23e6");
 
     if(moment(date).isValid()){
-      return moment(date).tz('Asia/Shanghai').format('MMM Do YY, h:mm:ss');
+      return moment(date).tz('Asia/Shanghai').format('MMM Do YY, h:mm:ss a');
     }else{
       return date;
     }
