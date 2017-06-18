b7fe4941797b8e69b803daa7411886a6a4bf6483
Steven Zhao <zhwdyx1201@gmail.com>
2017-06-18 13:32:41 +0800

    fix bug in the signing token process

--- routes/authentication.js
+++ routes/authentication.js
@@ -12,15 +12,16 @@ authRouter.post('/', function(req, res, next){
 		var inputKey = req.body.key;
 		// valid the key
 		keys.find({key: inputKey}, function(err, data){
-			if(err){
+			if(data.length){
+				jwt.sign(data[0].key, req.app.get('secret'), function(err, token){
+					if(err) res.status(500).send('Token can not be generated');
+					if(token){
+						res.status(200).send(token);
+					}
+				});
+			}else{
 				res.status(400).send('Password is incorrect');
 			}
-			jwt.sign(res.app.get('secret'), data[0].key, function(err, token){
-				if(err) res.status(500).send('Token can not be generated');
-				if(token){
-					res.status(200).send(token);
-				}
-			});
 		});
 	}else{
 		res.status(400).send('Key can not be empty');
